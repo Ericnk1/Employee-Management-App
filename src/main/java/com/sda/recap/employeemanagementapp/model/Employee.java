cb555273f34e9ab9ff2cc2592fe39d2844214605
Eric Ngako <ericnk1@live.com>
2021-03-27 21:43:52 +0200

    updated

--- src/main/java/com/sda/recap/employeemanagementapp/model/Employee.java
+++ src/main/java/com/sda/recap/employeemanagementapp/model/Employee.java
@@ -4,15 +4,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Data
 @NoArgsConstructor
 @Table(name = "employees")
-public class Employee {
+public class Employee implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(nullable = false, updatable = false)
     private long id;
 
     @Column(name = "first_name")
@@ -22,4 +25,6 @@ public class Employee {
     private String lastName;
 
     private String email;
+    private String jobTitle;
+    private String imageUrl;
 }
